9de52b56376b6fa62a4926249f6e4bc58cb23785
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-17 12:32:12 +0530

    Update telegraphurl.py

--- telegraphurl.py
+++ telegraphurl.py
@@ -72,35 +72,7 @@ async def get_link_group(client, message):
             return         
     except Exception:
         pass                                           
-        """
-    await startips.send_message(message.chat.id, text, disable_web_page_preview=True)
-
-
-@startips.on_message(filters.command('id'))
-async def get_id(client, message):
-    try:
-
-        if (not message.reply_to_message) and (message.chat):
-            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.\nThis chat's ID is: <code>{message.chat.id}</code>.") 
-
-        elif not message.reply_to_message:
-            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.") 
-
-        elif message.reply_to_message.forward_from_chat:
-            await message.reply(f"The forwarded {str(message.reply_to_message.forward_from_chat.type)[9:].lower()}, {message.reply_to_message.forward_from_chat.title} has an ID of <code>{message.reply_to_message.forward_from_chat.id}</code>.") 
-
-        elif message.reply_to_message.forward_from:
-            await message.reply(f"The forwarded user, {message.reply_to_message.forward_from.first_name} has an ID of <code>{message.reply_to_message.forward_from.id   }</code>.")
-
-        elif message.reply_to_message.forward_sender_name:
-            await message.reply("Sorry, you cannot get the forwarded user ID because of their privacy settings")
-
-        else:
-            await message.reply(f"User {message.reply_to_message.from_user.first_name}'s ID is <code>{message.reply_to_message.from_user.id}</code>.")   
-
-    except Exception:
-            await message.reply("An error occured while getting the ID.")
-
+        
 print("STAR Bot is alive!")
 startips.run()
 
